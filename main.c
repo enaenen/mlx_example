a5c16a659f2a20bf02351dc7ea69f4f177619494
SpaceChae <enaenen@naver.com>
2022-07-22 22:38:50 +0900

    o

--- main.c
+++ main.c
@@ -23,12 +23,19 @@ typedef struct s_param {
     int x;
     int y;
     char    str[3];
-}   t_param;
+}           t_param;
+
+typedef struct s_img{
+    void    *img;
+    int     *img_width;
+    int     *img_height;
+}           t_img;
 
 typedef struct s_mlx{
     void    *mlx;
     void    *win;
-}t_mlx;
+    t_img   *img;
+}              t_mlx;
 
 void    param_init(t_param *param)
 {
@@ -43,7 +50,7 @@ void    ft_mlx_init(t_mlx *_mlx)
 {
     _mlx->mlx = mlx_init();
     _mlx->win = mlx_new_window(_mlx->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
-
+    _mlx->img = malloc(sizeof(t_img));
 }
 
 int     key_press(int keycode, t_param *param)
@@ -60,6 +67,11 @@ int     key_press(int keycode, t_param *param)
     return (0);
 }
 
+void    ft_mlx_img_add(t_mlx *mlx)
+{
+    mlx->img->img = mlx_xpm_file_to_image(mlx->mlx, "../textures/wall_e.xpm", mlx->img->img_width, mlx->img->img_height);
+}
+
 int main(void)
 {
     t_mlx    mlx;
@@ -67,6 +79,7 @@ int main(void)
 
     param_init(&param);
     ft_mlx_init(&mlx);
+    ft_mlx_img_add(&mlx);
     printf("==============================\n");
     printf("W key : Add 1 to x.\n");
     printf("S key : Minus 1 to x.\n");
@@ -75,5 +88,6 @@ int main(void)
     printf("==============================\n");
     printf("Current x = 3\n");
     mlx_hook(mlx.win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+    mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img->img, 50, 50);
     mlx_loop(mlx.mlx);
 }
